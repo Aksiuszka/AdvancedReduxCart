208429e68f00de2e6120458c81db2fee5f7f879c
Anna Kawecka <aakawecka89@gmail.com>
2022-10-20 08:16:09 +0200

    notification

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux/es/exports';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -5,7 +6,16 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCard = useSelector(state => state.ui.isVisible);
+  const cart = useSelector(state => state.card);
   console.log(showCard);
+
+  useEffect(() => {
+    fetch('https://cart-40a4c-default-rtdb.firebaseio.com/', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCard && <Cart />}
